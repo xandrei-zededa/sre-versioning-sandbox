575e41f3f61d15145089dbdbea300f7eb440077b
xandrei-zededa <xandrei@zededa.com>
2026-09-22 20:06:05 +0200

    fix(deps): remove duplicate outputs across main and outputs tf

--- terraform-modules/parts/aws/irsa_role/outputs.tf
+++ terraform-modules/parts/aws/irsa_role/outputs.tf
@@ -1,13 +1,3 @@
-output "role_arn" {
-  description = "ARN of IAM role"
-  value       = aws_iam_role.this.arn
-}
-
-output "role_name" {
-  description = "Name of IAM role"
-  value       = aws_iam_role.this.name
-}
-
 output "permissions_boundary_applied" {
   description = "Multi-package boundary flag"
   value       = true
