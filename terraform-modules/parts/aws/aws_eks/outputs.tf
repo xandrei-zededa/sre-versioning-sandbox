517f122a9254158d8bc7b8e67ea9c5bdc089fb58
xandrei-zededa <xandrei@zededa.com>
2026-09-22 20:07:01 +0200

    feat(deps): atomic update to aws-eks and irsa-role modules (#36)
    * feat(deps): atomic update to aws-eks and irsa-role modules
    
    * fix(deps): remove duplicate outputs across main and outputs tf

---
+++ terraform-modules/parts/aws/aws_eks/outputs.tf
@@ -0,0 +1,4 @@
+output "vpc_cni_addon_enabled" {
+  description = "Multi-package feature flag"
+  value       = true
+}
---
+++ terraform-modules/parts/aws/irsa_role/outputs.tf
@@ -0,0 +1,4 @@
+output "permissions_boundary_applied" {
+  description = "Multi-package boundary flag"
+  value       = true
+}
